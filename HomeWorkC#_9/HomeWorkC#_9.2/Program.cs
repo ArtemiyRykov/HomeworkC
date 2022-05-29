4053fe368e70b5c106b81332d312a01d77f02e0e
Artemiy <azazellorart@gmail.com>
2022-05-29 22:53:21 +0500

    HomeWorkC#_9.2

---
+++ HomeWorkC#_9/HomeWorkC#_9.2/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+int m = Input("Введите значение M: ");
+int n = Input("Введите значение N: ");
+
+int Input(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int result = NumberSum(n, m);
+Console.WriteLine(result + " ");
+
+int NumberSum(int n, int m)
+{
+    if (n <= m)
+    {
+        return n;
+    }
+    return n + NumberSum(n - 1, m);
+}
